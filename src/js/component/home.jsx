aff0b8b8e3105b776a3cd683125c9df3aeb9793a
pablotori98 <pablotori98@gmail.com>
2022-10-29 18:17:01 +0000

    Primera entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import { array } from "prop-types";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+// const tasks = ["Task1", "Task2", "Task3", "Task4"]
 //create your first component
+
 const Home = () => {
+
+	const [input, setInput] = useState("")
+	const [tasks, setTasks] = useState([""])
+	const handleChange = (event) =>{
+		setInput(event.target.value) //Aqui estará el valor del imput, esto es una propiedad de javascript
+	}
+	const addInput = ()=>{
+		console.log("funciona")
+		setTasks(tasks.concat(input))
+	}
+	const deleteInput =()=>{
+		setTasks([])
+	}
+// const borrar = (index)=>{
+// 					setTasks(tasks.filter((task, id)=> id !== index)
+// 				}
 	return (
+		<>
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Taks list</h1>
+			<input type="text" name="" id="" onChange={handleChange}/>
+			<button onClick={addInput}>Añadir</button>
+			<p>El valor del input es {input} </p>
+			
+			{tasks.map((element, index)=>{
+				
+
+				return <p>{element}</p>
+			})}
+			
+
+			<button onClick={deleteInput}>Delete</button>
 		</div>
+		</>
 	);
 };
 
